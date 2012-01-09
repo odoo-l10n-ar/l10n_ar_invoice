792c4bac2bc1433c088054c8651543c667e30b90
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2012-01-09 11:49:25 -0300

    [FIX] bad formula to filter VAT

--- invoice.py
+++ invoice.py
@@ -23,61 +23,7 @@ from osv import fields, osv
 import decimal_precision as dp
 
 _all_taxes = lambda x: True
-_all_except_vat = lambda x: x not in ['IVA Ventas 21%', 'IVA Compras 21%']
-
-class account_tax(osv.osv):
-    """
-    Esta extensión resuelve el problema de mezclar impuestos sobre el valor agregado y 
-    sobre los ingresos brutos. El problema del código original es que al estar incluido
-    el impuesto al ingreso bruto en el valor del precio, el IVA se calcula sobre el
-    precio sin el impuesto al valor agregado. En argentina se lo aplica al
-    precio de lista sin importar los impuestos involucrados.
-    """
-    _inherit = "account.tax"
-
-    _columns = {
-        'belong_to_gross_price': fields.boolean('Tax belong to gross price'),
-    }
-
-    def compute_all(self, cr, uid, taxes, price_unit, quantity, address_id=None, product=None, partner=None):
-        """
-        RETURN: {
-                'total': 0.0,                # Total without taxes
-                'total_included: 0.0,        # Total with taxes
-                'taxes': []                  # List of taxes, see compute for the format
-            }
-        """
-        precision = self.pool.get('decimal.precision').precision_get(cr, uid, 'Account')
-        totalin = totalex = round(price_unit * quantity, precision)
-        tin = []
-        tex = []
-        gross = []
-        for tax in taxes:
-            if tax.price_include:
-                tin.append(tax)
-            else:
-                tex.append(tax)
-            if tax.belong_to_gross_price:
-                gross.append(tax.id)
-        tin = self.compute_inv(cr, uid, tin, price_unit, quantity, address_id=address_id, product=product, partner=partner)
-        for r in tin:
-            totalex -= r.get('amount', 0.0)
-        totlex_qty = 0.0
-        try:
-            totlex_qty=totalex/quantity
-        except:
-            pass
-        tex = self._compute(cr, uid, tex, totlex_qty, quantity, address_id=address_id, product=product, partner=partner)
-        for r in tex:
-            if not r['id'] in gross:
-                totalin += r.get('amount', 0.0)
-        print totalex, totalin, tin + tex
-        return {
-            'total': totalex,
-            'total_included': totalin,
-            'taxes': tin + tex
-        }
-account_tax()
+_all_except_vat = lambda x: x.name not in ['IVA Ventas 21%', 'IVA Compras 21%']
 
 class account_invoice_line(osv.osv):
     """
@@ -99,7 +45,7 @@ class account_invoice_line(osv.osv):
                                         partner=line.invoice_id.partner_id)
             res[line.id] = taxes['total_included']
             if line.invoice_id:
-                cur = line.invoice_id.currency_id
+                cureturn line.invoice_id.currency_id
                 res[line.id] = cur_obj.round(cr, uid, cur, res[line.id])
         return res
 
