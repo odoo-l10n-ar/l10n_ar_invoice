641b57f59ff0a7c5b8ed190db43169036abdb54a
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2012-10-15 17:21:35 -0300

    [FIX] Actualización de las licencias de GPL-v3 a AGPL-v3

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
     'author':   'OpenERP - Team de Localización Argentina',
     'category': 'Localization/Argentina',
     'website':  'https://launchpad.net/~openerp-l10n-ar-localization',
-    'license':  'GPL-3',
+    'license':  'AGPL-3',
     'description': """
 Módulo de Facturación de la localización Argentina
 
