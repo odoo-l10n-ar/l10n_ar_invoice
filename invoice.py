a265af8a8e061e674ee80dddd381ceaf23f5b67d
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2014-12-08 11:11:39 -0300

    [FIX] Wrong indentation for responsability check.

--- invoice.py
+++ invoice.py
@@ -167,10 +167,11 @@ class account_invoice(osv.osv):
             partner = self.env['res.partner'].browse(partner_id)
             company = self.env['res.company'].browse(company_id)
             responsability = partner.responsability_id
-	    if not responsability:
-                result['warning']={'title': _('The partner has not set any fiscal responsability'),
-                                   'message': _('Please, set partner fiscal responsability in the partner form before continuing.')}
-                return result
+
+            if not responsability:
+                    result['warning']={'title': _('The partner has not set any fiscal responsability'),
+                                       'message': _('Please, set partner fiscal responsability in the partner form before continuing.')}
+                    return result
 
             if responsability.issuer_relation_ids is None:
                 return result
