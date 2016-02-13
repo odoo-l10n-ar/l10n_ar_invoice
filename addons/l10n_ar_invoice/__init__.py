9431c024fe449a2d508dd96db15c0a03e2fde399
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2016-02-13 09:37:19 -0300

    [REF] Compatible with Odoo Guidelines

--- addons/l10n_ar_invoice/__init__.py
+++ addons/l10n_ar_invoice/__init__.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-import test
-import afip
-import invoice
-import config
-import account
-import currency
-import country
-import partner
+
+import models
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ addons/l10n_ar_invoice/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import afip
+import invoice
+import config
+import account
+import currency
+import country
+import partner
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
